e9f960f53e8e0b0c8a5ea0fecd8c8f41b3f700b4
Yeghishe Piruzyan <ypiruzyan@gmail.com>
2017-01-26 22:15:06 -0800

    small improvment

--- src/main/scala/io/github/yeghishe/Config.scala
+++ src/main/scala/io/github/yeghishe/Config.scala
@@ -3,10 +3,14 @@ package io.github.yeghishe
 import com.typesafe.config.ConfigFactory
 import net.ceedubs.ficus.Ficus
 import net.ceedubs.ficus.readers.ArbitraryTypeReader
+import net.ceedubs.ficus.readers.namemappers.implicits
 
 trait Config {
   import Ficus._
   import ArbitraryTypeReader._
+  import implicits.hyphenCase
 
   private val config = ConfigFactory.load()
 }
+
+object Config extends Config
